dfb0bc34efd398469ccf4daac70292c15f8ade18
Eleonore Baillot <eleonorebaillot@gmail.com>
2024-02-06 16:04:09 +0100

    new, create and show actions implemented for Games Controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,25 @@
 class GamesController < ApplicationController
+
   def show
     @game = Game.find(params[:id])
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(params[:game])
+    if @game.save!
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'games/show'
-  get 'games/new'
-  get 'games/create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'games#new'
   resources :games, only: %i[new create show]
 end
